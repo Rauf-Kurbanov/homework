26fc7c33009055807d6151710e184c43832646c8
parradox <rauf.kurbanov@student.spbu.ru>
2012-03-05 21:43:33 +0300

    stack for the task4.2

---
+++ semester_2/hw_4/Task4.2/src/main/AbstractStack.java
@@ -0,0 +1,17 @@
+package main;
+
+public abstract class AbstractStack implements StackInterface {
+    protected int size = 0;
+    
+    @Override
+    public int size() {
+        return size;
+    }
+    
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    
+    //abstract public 
+}
