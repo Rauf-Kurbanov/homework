d9dbc41b066848164d4a5a1a0bb79bf64eace8f6
parradox <rauf.kurbanov@student.spbu.ru>
2013-03-06 05:47:17 +0300

    Task3.6

---
+++ semester_4/hw_3/Task3.6/Task3.6/Program.fs
@@ -0,0 +1,11 @@
+﻿let isPrime (n : bigint) =
+    seq { bigint(2) .. bigint(sqrt(float n))}
+        |> Seq.exists (fun x -> n % x = bigint(0)) 
+        |> not
+
+let primes =
+    Seq.initInfinite (fun i -> i + 2)
+    |> Seq.map (fun i -> bigint(i))
+    |> Seq.filter isPrime
+
+printfn "%A" primes
