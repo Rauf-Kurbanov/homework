dee357c7135bbdbe72aa374be2ec7b4037686dad
parradox <rauf.kurbanov@student.spbu.ru>
2013-03-28 23:25:46 +0300

    4.1 and 4.5

---
+++ semester_4/hw_4/Task4.1/Task4.1/Program.fs
@@ -0,0 +1,20 @@
+﻿open System.Collections.Generic
+
+let explode (s:string) = [for c in s -> c]
+let checkBracket (str : string) =
+    let stack = new Stack<char>()
+    stack.Push('0')
+    let rec loop str =
+        match str with
+        | [] -> true
+        | h :: t -> match h with    
+                    | '(' | '[' | '{' -> stack.Push(str.[0])
+                                         loop(t)
+                    | ')' -> if (stack.Pop() <> '(') then false else loop(t)
+                    | ']' -> if (stack.Pop() <> '[') then false else loop(t)
+                    | '}' -> if (stack.Pop() <> '{') then false else loop(t)
+                    | _ -> loop(t)
+    loop (explode str)
+
+printf "%b\n" (checkBracket "ааааа[({)}]а")
+printf "%b\n" (checkBracket "bite( [{} [] m]e)")
---
+++ semester_4/hw_4/Task4.5/Task4.5/Program.fs
@@ -0,0 +1,12 @@
+﻿type Tree<'a> =
+    | Tree of 'a * Tree<'a> * Tree<'a>
+    | Tip of 'a
+
+    member t.map f =
+        match t with
+        | Tree(value, l, r) -> Tree(f value, l.map f, r.map f)
+        | Tip(value) -> Tip(f value)
+
+let testTree = Tree(1, Tree(2, Tip(7), Tip(8)), Tip(3))
+printf "%A\n" testTree
+printf "%A\n" (testTree.map (fun x -> x * 10))
